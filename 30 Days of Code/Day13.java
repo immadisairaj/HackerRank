9388859daf6176243fe70cfcbaf4d0bbb2e4b534
immadisairaj <immadirajendra.sai@gmail.com>
2018-07-10 17:59:08 +0530

    Day 12,13 ,30 Days of Code

---
+++ 30 Days of Code/Day13.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+abstract class Book {
+    String title;
+    String author;
+    
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+    
+    abstract void display();
+}
+
+class MyBook extends Book {
+    private int Price; // Declare the price instance variable
+    
+    MyBook(String title, String author, int Price){
+        super(title,author);
+        this.Price = Price;
+    }
+    
+    public void display() {
+        System.out.println("Title: "+super.title);
+        System.out.println("Author: "+super.author);
+        System.out.println("Price: "+Price);
+    }
+    
+}
+
+public class Day13 {
+   
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+}
