bf5a470c6ba8c9ccd74ff33048919c8a2f516948
immadisairaj <immadirajendra.sai@gmail.com>
2018-07-16 14:45:00 +0530

    30 Days of Code Day 19,20,21

---
+++ 30 Days of Code/Day20.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Day20 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        
+        int count = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int numberOfSwaps = 0;
+    
+            for (int j = 0; j < n - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                    numberOfSwaps++;
+                    count++;
+                }
+            }
+    
+            if (numberOfSwaps == 0) {
+                break;
+            }
+        }
+        
+        System.out.println("Array is sorted in "+count+" swaps.");
+        System.out.println("First Element: "+a[0]);
+        System.out.println("Last Element: "+a[n-1]);
+    }
+}
